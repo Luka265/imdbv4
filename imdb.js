b1f85359196d4f015864f07f0bae2ab67d961454
Luka265 <ghanishashvililuka@gmail.com>
2025-09-04 17:22:50 +0400

    Add files via upload

--- imdb.js
+++ imdb.js
@@ -1,5 +1,15 @@
 class Movies{
     getMovieList (){
-        
+       function Movie(id, name, description, photo, rating) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.photo = photo;
+        this.rating = rating;
+       }
+    
+       const strangers = new Movie("127", "9Stranger", "Description", "X", "4.8")
+       return strangers.name + strangers.id + strangers.rating
     }
+
 }
